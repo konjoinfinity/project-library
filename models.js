1f888be9a4d2d538ef03bc6c772b7fe662c4b39a
Wesley Scholl <clients@konjoweb.com>
2023-02-28 16:51:37 -0500

    added commentcount to model

--- models.js
+++ models.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const BookSchema = new Schema({
   title: { type: String, required: true },
   comments: [String],
+  commentcount: Number
   // String array inside of book.comments 
 });
 const Book = mongoose.model("Book", BookSchema);
